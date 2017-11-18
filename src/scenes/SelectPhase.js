ea62758d9e7045b59a9aa8091e47907cba01fccf
Henrik Karlsson <henrik.karlsson.88@live.se>
2017-11-18 04:00:11 +0100

    Working with bootstrap

--- src/scenes/SelectPhase.js
+++ src/scenes/SelectPhase.js
@@ -3,9 +3,15 @@
  */
 import React, {Component} from 'react';
 import '../App.css';
-import {Button, Grid, Row, Col, Glyphicon} from 'react-bootstrap';
+import '../popup.css';
+import {Button, Grid, Row, Col} from 'react-bootstrap';
 import CardCarousel from '../components/CardCarousel';
 import chasLogo from '../assets/chas-logo-white-512.png';
+import Popup from 'react-popup';;
+
+
+var helpText = "Pick a card from the slider and then click 'Play' when you are ready to proceed and plan with your friends." +
+    " " + "Pick a card by dragging or clicking the dots below the deck";
 
 class SelectPhase extends Component {
     render() {
@@ -17,7 +23,7 @@ class SelectPhase extends Component {
                         <Col xs={6} md={4}><img style={{width: "20%"}} src={chasLogo}/></Col>
                         <Col xs={6} md={4}><h1 className="App-title">Planning Poker</h1></Col>
                         <Col xsHidden md={4}>
-                            <Button bsStyle="primary">Play!</Button>
+                            <Button bsStyle="primary">Play now!</Button>
                         </Col>
                     </Row>
                         </header>
@@ -34,9 +40,20 @@ class SelectPhase extends Component {
                         <br/>
                     </Row>
                     <Row className="showGrid">
-                        <Button bsStyle="primary" >In need of help?</Button>
+                        <Button bsStyle="primary" onClick={() => Popup.alert(helpText, "Planning Poker", true)}>
+                            Help <span className="glyphicon glyphicon-question-sign" aria-hidden="true"> </span>
+                        </Button>
                     </Row>
                 </Grid>
+                <Popup
+                    className="mm-popup"
+                    btnClass="mm-popup__btn"
+                    closeBtn={true}
+                    closeHtml={null}
+                    defaultOk="Ok"
+                    defaultCancel="Cancel"
+                    wildClasses={false}
+                    closeOnOutsideClick={true} />
             </div>
         );
     }
