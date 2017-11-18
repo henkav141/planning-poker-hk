9abe977cc47a848c6f4accf42648a6b2a3886317
Henrik Karlsson <henrik.karlsson.88@live.se>
2017-11-18 01:06:15 +0100

    added cards

--- src/App.js
+++ src/App.js
@@ -8,15 +8,14 @@ class App extends Component {
   render() {
     return (
       <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h1 className="App-title">Welcome to React</h1>
-        </header>
-        <p className="App-intro">
-            <CardCarousel/>
-            <br/>
-            <Button bsStyle="success">Chose card</Button>
-        </p>
+          <header className="App-header">
+              <img src={logo} className="App-logo" alt="logo" />
+              <h1 className="App-title">Planning Poker</h1>
+          </header>
+
+          <CardCarousel/>
+          <br/>
+          <Button bsStyle="success">Chose card</Button>
       </div>
     );
   }
--- src/components/CardCarousel.js
+++ src/components/CardCarousel.js
@@ -7,9 +7,12 @@ import zeroCard from '../assets/cards/0-card.jpg';
 import zeroFiveCard from '../assets/cards/05-card.jpg';
 import oneCard from '../assets/cards/1-card.jpg';
 import twoCard from '../assets/cards/2-card.jpg';
+import fiveCard from '../assets/cards/5-card.jpg';
+import eightCard from '../assets/cards/8-card.jpg';
+import thirteenCard from '../assets/cards/13-card.jpg';
+import twentyCard from '../assets/cards/20-card.jpg';
 
 var settings = {
-    arrows: true,
     dots: true,
     infinite: true,
     speed: 500,
@@ -23,18 +26,19 @@ var backOfCard = "https://i.imgur.com/AOXbekl.jpg";
 class CardCarousel extends Component {
     render() {
         return (
-            <Slider {...settings}>
-                <div><img style={{margin: "auto"}} src={zeroCard} /></div>
-                <div><img style={{margin: "auto"}} src={zeroFiveCard} /></div>
-                <div><img style={{margin: "auto"}} src={oneCard} /></div>
-                <div><img style={{margin: "auto"}} src={twoCard} /></div>
-                <div><img style={{margin: "auto"}} src={zeroCard} /></div>
-                <div><img style={{margin: "auto"}} src={zeroFiveCard} /></div>
-                <div><img style={{margin: "auto"}} src={oneCard} /></div>
-                <div><img style={{margin: "auto"}} src={twoCard} /></div>
-            </Slider>
+                <Slider {...settings}>
+                    <div><img style={{margin: "auto"}} src={zeroCard}/></div>
+                    <div><img style={{margin: "auto"}} src={zeroFiveCard}/></div>
+                    <div><img style={{margin: "auto"}} src={oneCard}/></div>
+                    <div><img style={{margin: "auto"}} src={twoCard}/></div>
+                    <div><img style={{margin: "auto"}} src={fiveCard}/></div>
+                    <div><img style={{margin: "auto"}} src={eightCard}/></div>
+                    <div><img style={{margin: "auto"}} src={thirteenCard}/></div>
+                    <div><img style={{margin: "auto"}} src={twentyCard}/></div>
+                </Slider>
         );
     }
-};
+}
+;
 
 export default CardCarousel;
