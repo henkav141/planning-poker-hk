de40f626480f2a001b6731f84ff76b6e638bb1a2
Henrik Karlsson <henrik.karlsson.88@live.se>
2017-11-19 19:54:43 +0100

    Added container directory

--- src/index.js
+++ src/index.js
@@ -1,7 +1,7 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
-import MainSceneContainer from './scenes/MainSceneContainer';
+import MainSceneContainer from './container/MainSceneContainer';
 import { BrowserRouter } from 'react-router-dom'
 import registerServiceWorker from './registerServiceWorker';
 
